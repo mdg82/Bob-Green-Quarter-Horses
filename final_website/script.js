fb01dc37d95aab204e63dc3be48f9bfaca052a06
mgreen82 <melanie.green19@gmail.com>
2021-01-16 20:05:07 -0600

    Added images, navbar changes

--- final_website/script.js
+++ final_website/script.js
@@ -19,5 +19,16 @@ function toggleMenu() {
 	var menu = document.querySelector('.menu');
 	menuToggle.classList.toggle('active');
 	menu.classList.toggle('active');
-}
+} 
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-175px";
+  }
+  prevScrollpos = currentScrollPos;
+}
