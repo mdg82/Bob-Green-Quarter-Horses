71910a8963236405406e232a9dfbc03ec865a952
mgreen82 <melanie.green19@gmail.com>
2021-05-18 21:55:35 -0500

    05182021 commit

--- final_website/script.js
+++ final_website/script.js
@@ -32,3 +32,50 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+  $(window).scroll(function() {
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+  
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+    /*$(".info").fadeIn(1000);
+    setTimeout(function(){
+      $(".info").fadeIn(1000);
+  }, 1000)*/
+  });
+});
+
+/*setTimeout(function(){
+  $(".info").fadeIn(1000);
+}, 1000)*/
+
+//onClick="window.location.href='service.html#section-1';"
+
+
